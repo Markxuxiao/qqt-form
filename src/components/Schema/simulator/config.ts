8befa99dea5f62eba3e05eb0a63a0bfaf48de0ed
Mark <mandk@qq.com>
2023-06-09 17:18:10 +0800

    优化

--- src/components/Schema/simulator/config.ts
+++ src/components/Schema/simulator/config.ts
@@ -7,8 +7,3 @@ export const CANVAS_COMPONENT_TAG = "data-comp-id";
  * 左侧导航组件展示列表item的标识，可拖入到画布中
  */
 export const MENU_COMPONENT_TAG = "data-comp-snippet";
-
-/**
- * 画布被选中组件操作按钮标识
- */
-export const CANVAS_COMPONENT_BTN_TAG = "data-btn";
